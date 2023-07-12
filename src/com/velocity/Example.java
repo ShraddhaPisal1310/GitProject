c43218d3f422d9a43d4171fd64ef465fc63803e6
shraddha <shraddhapisal32@gmail.com>
2023-07-12 21:09:05 +0530

    Second commit

--- src/com/velocity/Example.java
+++ src/com/velocity/Example.java
@@ -6,6 +6,7 @@ public class Example {
 		// TODO Auto-generated method stub
              
 		System.out.println("This is Example class");
+		System.out.println("This is newely generated sout");
 	}
 
 }

0a81b430da50fc6dffd150a923ac1ac3346e74e0
Shraddha Lokhande <shraddha.ingale29@gmail.com>
2023-07-31 23:18:20 +0530

    Added one statement good evening

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,8 +4,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ");
-		
-		   System.out.println("Welcome to java");
+		System.out.println("Welcome to java");
+		System.out.println("good evening ");
 	}
 
 }
